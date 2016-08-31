254f283d67a8fca4e2a36d0656441cc72daa6788
MIGUEL MANZANO GARCÍA <mmanzano@gmail.com>
2016-08-31 14:54:01 +0200

    Google JS API Key

--- resources/views/scripts/head.blade.php
+++ resources/views/scripts/head.blade.php
@@ -8,6 +8,6 @@
     {{ trans('pokemonbuddy.site_name') }}
 </title>
 <link href="{{ asset('/css/pokemonbuddy.css') }}" rel="stylesheet">
-<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAPS_API_JS_KEY') }}&v=3.exp&libraries=places"></script>
 
 
